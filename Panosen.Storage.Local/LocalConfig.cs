b2d95da77b805c8a254b2f13c2d41732538c1871
hc_zhang <hc_zhang@trip.com>
2021-08-05 12:57:16 +0800

    add Panosen.Storage.Local.csproj

---
+++ Panosen.Storage.Local/LocalConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Panosen.Storage.Local
+{
+    /// <summary>
+    /// 本地配置
+    /// </summary>
+    public class LocalConfig
+    {
+        /// <summary>
+        /// 文件夹
+        /// </summary>
+        public string Folder { get; set; }
+    }
+}
---
+++ Panosen.Storage.Local/LocalStorage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Panosen.Storage.Local
+{
+    /// <summary>
+    /// 保存文件到本地
+    /// </summary>
+    public class LocalStorage
+    {
+        private readonly LocalConfig localConfig;
+
+        /// <summary>
+        /// 保存文件到本地
+        /// </summary>
+        public LocalStorage(LocalConfig qiniuConfig)
+        {
+            this.localConfig = qiniuConfig;
+        }
+
+        /// <summary>
+        /// 保存文件
+        /// </summary>
+        public void Save(string key, byte[] bytes)
+        {
+            var path = Path.Combine(localConfig.Folder, key);
+
+            var folder = Path.GetDirectoryName(path);
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            File.WriteAllBytes(path, bytes);
+        }
+    }
+}
